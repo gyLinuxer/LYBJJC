bc1592fc027bda86974b4fcb806d957cb80bbcf7
root <root@peixun.(none)>
2019-03-02 09:50:27 +0800

    190302-1

---
+++ application/safetymng/controller/TaskList.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liguangyao
+ * Date: 2018/12/4
+ * Time: 20:31
+ */
+namespace app\safetymng\controller;
+use think\Controller;
+use think\Db;
+
+
+class TaskList extends PublicController
+{
+    public function Index()
+    {
+        $CorpRole = session('CorpRole');
+        $TaskList = '';
+        if($CorpRole=='领导'){
+            $TaskList = db()->query("SELECT * FROM TaskList WHERE isDeleted = '否' AND  ReciveCorp = ?",array(session("Corp")));
+        }else{
+            $TaskList = db()->query("SELECT * FROM TaskList WHERE isDeleted = '否' AND  id in 
+                                (SELECT DISTINCT TaskID FROM TaskDealerGroup WHERE Name=?)",array(session('Name')));
+        }
+        $this->assign("TaskList",$TaskList);
+        $this->assign("Cnt",1);
+        return view('index');
+    }
+}
