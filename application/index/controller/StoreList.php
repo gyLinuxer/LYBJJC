e2a8ebfd85c4c78c95f68ce2a298ba97cac7ed46
MAC <gylinuxer@gmail.com>
2019-06-03 23:06:51 +0800

    增加商户修改

--- application/index/controller/StoreList.php
+++ application/index/controller/StoreList.php
@@ -76,4 +76,79 @@ class StoreList extends PublicController{
         OUT:
             return $this->index();
     }
+
+    public function showMdfStore($StoreCode=NULL){
+        $Ret = db('StoreList')->where(array('StoreCode'=>$StoreCode))->find();
+        if(empty($Ret)){
+            return 'FR,这个商户号好像不存在啊！';
+        }
+
+        $this->assign('Store',$Ret);
+
+        $this->assign("StoreAddrList",db('StoreAddr')->order("addrcode ASC")->select());
+
+        return view("StoreMdf");
+    }
+
+    public function MdfStore(){
+        $StoreName = trim(input("StoreName"));
+        $StoreCode = trim(input("StoreCode"));
+        $StoreAddr = trim(input("StoreAddr"));
+        $StoreOwner = trim(input("StoreOwner"));
+        $Tel = trim(input("Tel"));
+        $StoreArea = input("StoreArea");
+        $StoreRental = input("StoreRental");
+        $NextGiveDate = input("NextGiveDate");
+        $ContractDate = input("ContactDate");
+        $ContractCode = input("ContactCode");
+        $file = request()->file("ContactFile");
+        $StartDu= floatval(input("StartDu"));
+
+        if(empty($StoreCode) || empty($NextGiveDate) || empty($StoreName)
+            || empty($StoreAddr) || empty($StoreOwner) || empty($Tel)
+            || empty($StoreRental) || empty($ContractDate) ){
+            $this->assign("Warning","请将项目填写完整!");
+            goto OUT;
+        }
+
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'upload');
+            if($info){
+                $data["ContactFileName"] = $info->getSaveName();
+            }else{
+                $this->assign("Warning","上传文件错误:".$file->getError());
+                goto OUT;
+            }
+        }
+
+        $where["StoreCode"] = $StoreCode;
+        $Ret = Db("StoreList")->where($where)->select();
+        if(empty($Ret)){
+            $this->assign("Warning","该商铺不存在!!");
+            goto OUT;
+        }
+
+        //$data["StoreCode"] = $StoreCode;
+        $data["StoreName"] = $StoreName;
+        $data["StoreAddr"] = $StoreAddr;
+        $data["StoreOwner"] = $StoreOwner;
+        $data["Tel"] = $Tel;
+        $data["StoreRental"] = $StoreRental;
+        $data["NextGiveDate"] = $NextGiveDate;
+        $data["StoreArea"] = $StoreArea;
+        $data["ContactDate"] = $ContractDate;
+        $data["ContactCode"] = $ContractCode;
+        $data["DFDeadDU"]   = $StartDu;
+        $data["DFDeadDate"] = date("Y-m-d");
+        $data["SFDeadDate"] = $this->GetTheLastDay();
+        $data["WYFDeadDate"]= $this->GetTheLastDay();
+        if(db("StoreList")->where(array('StoreCode'=>$StoreCode))->update($data)>0){
+            $this->assign("Warning","店铺信息修改成功！!");
+        }
+        OUT:
+        return $this->showMdfStore($StoreCode);
+    }
+
+
+
 }
